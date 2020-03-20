6aa4c045d182989566556451045e638d81c426af
wDaniec <wdaniec@plaster.capdnet>
2020-03-20 12:28:39 +0100

    Basic working code for cifar and imagenet

---
+++ code.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import neptune
+
+
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+BATCH_SIZE = 92
+TRAIN_NAME = "cifar"
+SEND_NEPTUNE = True
+NEPTUNE_NAME = "cifar10"
+OUT_SIZE = 10
+
+if SEND_NEPTUNE:
+    neptune.init('andrzejzdobywca/pretrainingpp')
+    neptune.create_experiment(name='minimal_example')
+
+data_dir = '~/data/lilImageNet'
+data_dir_cifar = '/mnt/remote/wmii_gmum_projects/datasets/vision'
+
+transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+# image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+#                                           transform=transform)
+#                   for x in ['train', 'val']}
+
+image_datasets = {'train': torchvision.datasets.CIFAR10(root='./data_dir_cifar', train=True, download=True, transform=transform), 
+              'val': torchvision.datasets.CIFAR10(root='./data_dir_cifar', train=False, download=True, transform=transform)}
+
+
+class FullTrainingDataset(torch.utils.data.Dataset):
+    def __init__(self, full_ds, offset, length):
+        self.full_ds = full_ds
+        self.offset = offset
+        self.length = length
+        assert len(full_ds)>=offset+length, Exception("Parent Dataset not long enough")
+        super(FullTrainingDataset, self).__init__()
+        
+    def __len__(self):
+        return self.length
+    
+    def __getitem__(self, i):
+        return self.full_ds[i+self.offset]
+    
+def trainTestSplit(dataset, val_share):
+    val_offset = int(len(dataset)*val_share)
+    return FullTrainingDataset(dataset, 0, val_offset), FullTrainingDataset(dataset, val_offset, len(dataset)-val_offset)
+
+ 
+train_ds, _ = trainTestSplit(image_datasets['train'], 0.1)
+
+image_datasets['train'] = train_ds
+
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=BATCH_SIZE,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'val']}
+
+
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+
+# class_names = image_datasets['train'].classes
+# print(class_names)
+print(len(image_datasets['val']))
+print(len(image_datasets['train']))
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for idx, (inputs, labels) in enumerate(dataloaders[phase]):
+#                 print(idx, len(dataloaders[phase]))
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+            
+            if SEND_NEPTUNE:
+                neptune.send_metric('{}_loss'.format(phase), epoch_loss)
+                neptune.send_metric('{}_acc'.format(phase), epoch_acc)
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+                torch.save(best_model_wts, "./{}_{}.pth".format(TRAIN_NAME, epoch))
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+model_ft = models.mobilenet_v2()
+num_ftrs = model_ft.classifier[1].in_features
+
+model_ft.classifier[1] = nn.Linear(num_ftrs, OUT_SIZE)
+
+model_ft = model_ft.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=25, gamma=0.1)
+
+
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=100)
+
+
+
+
+
