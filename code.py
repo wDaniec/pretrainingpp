51933c7cb65144735e1348999359e510f5f5df92
wDaniec <andrzejZdobywca@gmail.com>
2020-04-05 18:41:10 +0200

    Added sending of config file and best .pth model to neptune

--- code.py
+++ code.py
@@ -5,9 +5,7 @@ from torch.optim import lr_scheduler
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
-import time
 import os
-import copy
 import neptune
 import sys
 import utils
@@ -18,137 +16,56 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 torch.manual_seed(0)
 
 opt = utils.getConfig()
-print(opt)
-
-print(opt.sendNeptune)
 
 if opt.sendNeptune:
     neptune.init('andrzejzdobywca/pretrainingpp')
     exp = neptune.create_experiment(name=opt.sessionName)
+    exp.log_artifact('config.txt')
 
-transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
+class Network():
+    def __init__(self):
+        model_ft = models.mobilenet_v2()
+        num_ftrs = model_ft.classifier[1].in_features
 
-if opt.dataset == "Cifar":
-    image_datasets = {'train': torchvision.datasets.CIFAR10(root='./data_dir_cifar', train=True, download=True, transform=transform), 
-                'val': torchvision.datasets.CIFAR10(root='./data_dir_cifar', train=False, download=True, transform=transform)}
-    train_ds, _ = utils.trainTestSplit(image_datasets['train'], opt.cifarFactor)
-    image_datasets['train'] = train_ds
-else:
-    image_datasets = {x: datasets.ImageFolder(os.path.join('~/data/lilImageNet', x),
-                                          transform=transform)
-                  for x in ['train', 'val']}
- 
+        model_ft.classifier[1] = nn.Linear(num_ftrs, 100)
+        if opt.pretrained:
+            model_ft.load_state_dict(torch.load(opt.net))
+        num_ftrs = model_ft.classifier[1].in_features
+        model_ft.classifier[1] = nn.Linear(num_ftrs, opt.outSize)
 
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batchSize,
-                                             shuffle=True, num_workers=4)
-              for x in ['train', 'val']}
+        self.model = model_ft.to(opt.device)
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(model_ft.parameters(), weight_decay=opt.weightDecay, lr=opt.lr)
 
 
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 
-print('val length:', len(image_datasets['val']))
-print('train length:', len(image_datasets['train']))
-device = torch.device("cuda:{}".format(opt.gpuId) if torch.cuda.is_available() else "cpu")
-# device = torch.device("cpu")
+class Dataset():
+    def __init__(self):
+        if opt.dataset == "Cifar":
+            image_datasets = {'train': torchvision.datasets.CIFAR10(root='./data_dir_cifar', train=True, download=True, transform=opt.transform), 
+                        'val': torchvision.datasets.CIFAR10(root='./data_dir_cifar', train=False, download=True, transform=opt.transform)}
+            train_ds, _ = utils.trainTestSplit(image_datasets['train'], opt.cifarFactor)
+            image_datasets['train'] = train_ds
+        else:
+            image_datasets = {x: datasets.ImageFolder(os.path.join('~/data/lilImageNet', x),
+                                                transform=opt.transform)
+                        for x in ['train', 'val']}
+        
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs):
-    folder_name = "experiments/{}".format(opt.sessionName)
-    if not os.path.exists(folder_name):
-        os.mkdir(folder_name)
+        self.dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batchSize,
+                                                    shuffle=True, num_workers=4) for x in ['train', 'val']}
+        
+        self.dataset_sizes = dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 
-    since = time.time()
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
-
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                model.train()  # Set model to training mode
-            else:
-                model.eval()   # Set model to evaluate mode
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            # Iterate over data.
-            for idx, (inputs, labels) in enumerate(dataloaders[phase]):
-#                 print(idx, len(dataloaders[phase]))
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-            # if phase == 'train':
-            #     scheduler.step()
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
-            
-            if opt.sendNeptune:
-                neptune.send_metric('{}_loss'.format(phase), epoch_loss)
-                neptune.send_metric('{}_acc'.format(phase), epoch_acc)
-
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
-
-        print()
-
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
-
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    torch.save(best_model_wts, "./{}/best_model.pth".format(opt.sessionName, epoch))
-    return model
+        print('val length:', len(image_datasets['val']))
+        print('train length:', len(image_datasets['train']))
 
 
-model_ft = models.mobilenet_v2()
-num_ftrs = model_ft.classifier[1].in_features
+dataset = Dataset()
+net = Network()
 
-model_ft.classifier[1] = nn.Linear(num_ftrs, 100)
-if opt.pretrained:
-    model_ft.load_state_dict(torch.load(opt.net))
+utils.train_model(opt, net, dataset)
 
-
-num_ftrs = model_ft.classifier[1].in_features
-model_ft.classifier[1] = nn.Linear(num_ftrs, opt.outSize)
-
-model_ft = model_ft.to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer_ft = optim.Adam(model_ft.parameters(), weight_decay=opt.weightDecay, lr=opt.lr)
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=25, gamma=0.1)
-
-# exp_lr_scheduler jest wykomentowany!
-model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=180)
-if SEND_NEPTUNE:
+if opt.sendNeptune:
     neptune.stop()
+    exit(1)
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import torch
 import argparse
+import time
+import copy
+import neptune
+from torchvision import transforms
 
 class FullTrainingDataset(torch.utils.data.Dataset):
     def __init__(self, full_ds, offset, length):
@@ -19,25 +23,118 @@ def trainTestSplit(dataset, val_share):
     val_offset = int(len(dataset)*val_share)
     return FullTrainingDataset(dataset, 0, val_offset), FullTrainingDataset(dataset, val_offset, len(dataset)-val_offset)
 
+def train_model(opt, net, dataset):
+    model = net.model
+    criterion = net.criterion
+    optimizer = net.optimizer
+    dataloaders = dataset.dataloaders
+    dataset_sizes = dataset.dataset_sizes
+
+    since = time.time()
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    best_epoch = 0
+
+    for epoch in range(opt.epochs):
+        print('Epoch {}/{}'.format(epoch, opt.epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for idx, (inputs, labels) in enumerate(dataloaders[phase]):
+#                 print(idx, len(dataloaders[phase]))
+                inputs = inputs.to(opt.device)
+                labels = labels.to(opt.device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+            
+            if opt.sendNeptune:
+                neptune.send_metric('{}_loss'.format(phase), epoch_loss)
+                neptune.send_metric('{}_acc'.format(phase), epoch_acc)
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_epoch = epoch
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f} achieved on model from epoch: {}'.format(best_acc, best_epoch))
+
+    # load best model weights
+    if opt.sendNeptune:
+        model.load_state_dict(best_model_wts)
+        torch.save(best_model_wts, "./best_model.pth")
+        neptune.send_artifact('best_model.pth')
+    return model
+
 
 def getConfig():
+    # naprawic to tak, zeby nie byly potrzebne dwa parsery.
     parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
     parser.add_argument('--dataset', required=True, help='Cifar | ImageNet')
     parser.add_argument('--batchSize', type=int, required = True)
     parser.add_argument('--sessionName', required = True)
     parser.add_argument('--pretrained', action='store_true')
     parser.add_argument('--net', )
-    parser.add_argument('--sendNeptune', action='store_true')
     parser.add_argument('--cifarFactor', type=float, required = True)
     parser.add_argument('--lr', type=float, required=True)
     parser.add_argument('--weightDecay', type=float, required=True)
     parser.add_argument('--epochs', type=int, required=True)
     parser.add_argument('--gpuId', type=int, required=True)
-    
+
+    parserShell = argparse.ArgumentParser()
+    parserShell.add_argument('--sendNeptune', action='store_true')
+    parserShell.add_argument('--pretrained', action='store_true')
 
     opt = parser.parse_args(['@config.txt'])
+    optShell = parserShell.parse_args()
+
+    opt.sendNeptune = optShell.sendNeptune
+    opt.pretrained = optShell.pretrained
+    opt.device = torch.device("cuda:{}".format(opt.gpuId) if torch.cuda.is_available() else "cpu")
     if opt.dataset == "Cifar":
         opt.outSize = 10
     else:
         opt.outSize = 100
+
+    opt.transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
     return opt
